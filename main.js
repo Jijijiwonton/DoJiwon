81a1c0d29e7e61cc1edf9dc7096dc58629790d6e
jiwonhwang <jijijiwonton@github.com>
2022-07-04 20:56:17 -0400

    DWPD-10 handle scrolling when cliking on a navbar menu

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove(navbarChangeMode);
   }
 });
+
+/**
+ * Handling a scroll action when clicking on the navbar menu
+ */
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+});
